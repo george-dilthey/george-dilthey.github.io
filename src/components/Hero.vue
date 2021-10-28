891fed921e94fa205056d15cd7dcc24a6e103b2b
George Dilthey <george.dilthey@gmail.com>
2021-10-28 11:28:11 -0400

    added hero transitions

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,19 +4,24 @@
             <div class="row hero-row d-flex">
                 <div class="col hero-left d-flex align-items-center">
                     <div class="w-100">
-                        <HeroNav v-if="$route.name!='Home'"/>
-                        <h1 v-else>Hey, I'm George.</h1>
+                        <transition appear name="fade" mode="out-in" >
+                            <HeroNav v-if="$route.name!='Home'"/>
+                            <h1 v-else>Hey, I'm George.</h1>
+                        </transition>
                     </div>
                 </div>
                 <div class="col d-flex hero-right align-items-center">
+                    <transition name="fade" mode="out-in" appear>
                     <HeroNav v-if="$route.name=='Home'"/>
-                    <div v-else>
-                        <transition name="fade" mode="out-in">
-                            <h3 key="dev" v-if="$route.name=='Developer'">I graduated the Flatiron Software Engineering Bootcamp in 2021 and have been developing ever since. With a strong background in web analytics, I’ve always got the end user in mind.</h3>
-                            <h3 key="mar" v-else-if="$route.name=='Marketer'">With almost a decade of experience, I’ve learned the ins and outs of technical marketing. I can help with anything from pixel deployment to custom analytics solutions.</h3>
-                            <h3 key="cel" v-else-if="$route.name=='Cellist'">I’ve played the cello for over 20 years. Its my passion and has helped shape my whole life. I’m currently performing with the Greenwich Village Orchestra.</h3>
-                        </transition>
-                     </div>
+                        <div v-else>
+                                <transition name="fade" mode="out-in" appear>
+                                <h3 key="dev" v-if="$route.name=='Developer'">I graduated the Flatiron Software Engineering Bootcamp in 2021 and have been developing ever since. With a strong background in web analytics, I’ve always got the end user in mind.</h3>
+                                <h3 key="mar" v-else-if="$route.name=='Marketer'">With almost a decade of experience, I’ve learned the ins and outs of technical marketing. I can help with anything from pixel deployment to custom analytics solutions.</h3>
+                                <h3 key="cel" v-else-if="$route.name=='Cellist'">I’ve played the cello for over 20 years. Its my passion and has helped shape my whole life. I’m currently performing with the Greenwich Village Orchestra.</h3>
+                            </transition>
+                        </div>
+                    </transition>
+
                 </div>
             </div>
         </div>
@@ -72,7 +77,7 @@ h3 {
 
 .fade-enter-active,
 .fade-leave-active {
-  transition: all 1s ease;
+  transition: all .5s ease;
 }
 
 .fade-enter-from,
