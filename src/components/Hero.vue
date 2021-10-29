378304dc97ba8e7f8fa344004f91a31961fa2718
George Dilthey <george.dilthey@gmail.com>
2021-10-29 00:02:57 -0400

    built out line functionality

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,24 +1,23 @@
 <template>
     <div class="d-flex align-items-center min-vh-100 hero">
+        <HeroLine :key="$route.fullPath"/>
         <div class="container-fluid">
-            <div class=line>
-                <div class="row d-flex">
-                    <div class="col d-flex align-items-center hero-left">
-                        <div class="w-100">
-                            <transition appear name="fade" mode="out-in" >
-                                <h1 v-if="CheckRouteName($route.name, `Home`)">Hey, I'm George.</h1>
-                            </transition>
-                        </div>
+            <div class="row d-flex">
+                <div class="col d-flex align-items-center hero-left">
+                    <div class="w-100">
+                        <transition appear name="fade" mode="out-in" >
+                            <h1 v-if="CheckRouteName($route.name, `Home`)">Hey, I'm George.</h1>
+                        </transition>
                     </div>
-                    <div class="col d-flex align-items-center">
-                        <HeroNav />
-                        <div class="short-desc">
-                            <transition appear name="fade" mode="out-in">
-                                <h3 v-if="CheckRouteName($route.name, `Developer`)">I graduated the Flatiron Software Engineering Bootcamp in 2021 and have been developing ever since. With a strong background in web analytics, I’ve always got the end user in mind.</h3>
-                                <h3 v-else-if="CheckRouteName($route.name, `Marketer`)">With almost a decade of experience, I’ve learned the ins and outs of technical marketing. I can help with anything from pixel deployment to custom analytics solutions.</h3>
-                                <h3 v-else-if="CheckRouteName($route.name, `Cellist`)">I’ve played the cello for over 20 years. Its my passion and has helped shape my whole life. I’m currently performing with the Greenwich Village Orchestra.</h3>
-                            </transition>
-                        </div>
+                </div>
+                <div class="col d-flex align-items-center">
+                    <HeroNav />
+                    <div class="short-desc">
+                        <transition appear name="fade" mode="out-in">
+                            <h3 v-if="CheckRouteName($route.name, `Developer`)">I graduated the Flatiron Software Engineering Bootcamp in 2021 and have been developing ever since. With a strong background in web analytics, I’ve always got the end user in mind.</h3>
+                            <h3 v-else-if="CheckRouteName($route.name, `Marketer`)">With almost a decade of experience, I’ve learned the ins and outs of technical marketing. I can help with anything from pixel deployment to custom analytics solutions.</h3>
+                            <h3 v-else-if="CheckRouteName($route.name, `Cellist`)">I’ve played the cello for over 20 years. Its my passion and has helped shape my whole life. I’m currently performing with the Greenwich Village Orchestra.</h3>
+                        </transition>
                     </div>
                 </div>
             </div>
@@ -28,10 +27,11 @@
 
 <script>
 import HeroNav from '@/components/HeroNav.vue'
+import HeroLine from '@/components/HeroLine.vue'
 
 export default {
   name: 'Hero',
-  components: { HeroNav },
+  components: { HeroNav, HeroLine },
   methods: {
     CheckRouteName: function (p, a) {
       return (a.includes(p))
@@ -50,10 +50,6 @@ export default {
 
 .row {
     height: 50vh;
-}
-
-.hero-left {
-    /* border-right: 1px solid #AEC6CF; */
 }
 
 .short-desc{
@@ -75,7 +71,7 @@ h3 {
 
 .fade-enter-active,
 .fade-leave-active {
-  transition: all .8s ease;
+  transition: all .5s ease;
 }
 
 .fade-enter-from,
---
+++ src/components/HeroLine.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="line"></div>
+</template>
+
+<script>
+export default {
+  name: 'HeroLine'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.line {
+    position: absolute;
+    width: 50%;
+    height: 75%;
+    border-right: 1px solid #AEC6CF;
+    animation: line-anim 2s
+}
+
+@keyframes line-anim {
+  0% { height: 75% }
+  25% { height: 0% }
+  100% { height: 75% }
+}
+
+</style>
