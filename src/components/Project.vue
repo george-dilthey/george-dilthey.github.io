fa6cd102f6576cd998ab26323723c48920d5b43a
George Dilthey <george.dilthey@gmail.com>
2022-02-28 13:55:29 -0500

    added carousel slides

---
+++ src/components/Project.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Hello Project 1
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Project',
+  components: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/components/ProjectsCarousel.vue
+++ src/components/ProjectsCarousel.vue
@@ -1,28 +1,59 @@
 <template>
-  <carousel :items-to-show="1.5">
-    <slide v-for="slide in 10" :key="slide">
-      {{ slide }}
-    </slide>
+    <Carousel>
+      <Slide v-for="slide in 10" :key="slide">
+        <div class="carousel__item">{{ slide }}</div>
+      </Slide>
 
-    <template #addons>
-      <navigation />
-      <pagination />
-    </template>
-  </carousel>
+      <template #addons>
+        <Navigation />
+        <Pagination />
+      </template>
+    </Carousel>
 </template>
 
 <script>
-// If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
 import 'vue3-carousel/dist/carousel.css'
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+// import Project from '@/components/Project.vue'
 
-export default {
-  name: 'ProjectsCarousel',
+export default ({
+  name: 'ProjectCarousel',
   components: {
+    // Project,
     Carousel,
     Slide,
     Pagination,
     Navigation
   }
-}
+})
 </script>
+
+<style>
+
+section.carousel {
+  margin: auto;
+  width: 75%;
+}
+
+.carousel__item {
+  min-height: 200px;
+  width: 100%;
+  background-color: var(--vc-clr-primary);
+  color:  var(--vc-clr-white);
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel__slide {
+  padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+  border: 5px solid white;
+}
+</style>
